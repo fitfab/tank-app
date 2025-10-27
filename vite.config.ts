82e47124346bf534a0449d3f7fb77c88c53e7131
Miguel Julio <fitfab@yahoo.com>
2025-10-27 12:40:20 -0400

    feat: add wrangler setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/solid-start/plugin/vite";
 import viteSolid from "vite-plugin-solid";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tsConfigPaths(),
     tanstackStart(),
     // solid's vite plugin must come after start's vite plugin
